eeacd88ae5ea4cf3fe2a68c7989568b05f595647
Tcaf351 <74075640+Tcaf351@users.noreply.github.com>
2022-05-14 21:03:50 +1000

    imported a few middleware packages

--- src/index.js
+++ src/index.js
@@ -5,6 +5,10 @@ const cors = require('cors');
 
 const app = express();
 
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 
 const PORT = process.env.PORT || 8000
 app.listen(() => console.log(`Server running on port: ${PORT}`));
